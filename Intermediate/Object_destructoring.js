8810760b8344c90f32fb794aa053477806f0f2d5
VIGNESH_MURARISHETTI <viggumurari555@gmail.com>
2025-10-26 22:32:24 +0530

    Learnt Object destructoring

---
+++ Intermediate/Object_destructoring.js
@@ -0,0 +1,20 @@
+/*
+
+*/
+
+const course = {
+    course_name : "JS full length course",
+    course_tutor : "Vignesh",
+    course_duration : "5 hours"
+}
+
+// Object Destructoring
+const {course_tutor:tutor} = course;
+const {course_name : naam} = course;
+const {course_duration : duration} = course;
+
+// console.log(course_tutor); // Vignesh
+console.log(tutor); // Vignesh
+console.log(naam); // JS full length course
+console.log(duration); // 5 hours
+
